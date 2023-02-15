252b1c65b607300a4795858871b37a18082bd474
Yurii <79467842+yuro4kaw@users.noreply.github.com>
2023-02-15 14:22:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var video;
+var display;
+var video2;
+window.onload = function () {
+  video = document.getElementById('videoPlayer');
+  video2 = document.getElementById('videoPlayer2');
+};
+
+function play() {
+  video.play();
+}
+
+function play1() {
+  video2.play();
+}
